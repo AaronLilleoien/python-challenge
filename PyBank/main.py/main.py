a6196b114de260e8d2bc7ba45c54a1e4e2cbe768
AaronLilleoien <116856699+AaronLilleoien@users.noreply.github.com>
2023-01-17 21:06:10 -0600

    Good day

--- PyBank/main.py/main.py
+++ PyBank/main.py/main.py
@@ -94,30 +94,7 @@ with open(file_to_output, "w") as txt_file:
     
     
     
-    #pl_array2 = []
-    #pl_array2 = pl_array
     
-    
-    #total_pl_change =  (pl_array[0]) - pl_array[-1]
-    #months_to_avg = num_months - 1
-    #total_pl_change_avg = round(total_pl_change/months_to_avg,2)
-    
-    
-    #print(f"Total: ${profit_loss}")
-    #print(pl_array)
-    #print(f"${total_pl_change_avg}")
-    
-    #print(pl_array2)
-   
-    #change_list = []
-    #x = 0
-    #for x in pl_array2:
-        #last_num = pl_array.pop(-1)
-        #new_last = pl_array[-1]
-        #change = new_last - last_num
-        #change_list.append(change)
-    
-    #print(change_list)
         
     
     
@@ -127,22 +104,5 @@ with open(file_to_output, "w") as txt_file:
 # In[ ]:
 
 
-greatest_change = 0
-   pl_array= [int(x) for x in pl_array]
-   print(pl_array)
-   #for x in pl_array:
-    #   if pl_array[x] - pl_array[x+1] > greatest_change:
-     #      print("higher")
-      # else:
-   result = 0
-   highest = 0 
-   print("")
-   for x in range(len(pl_array)):
-       last_row = x
-   
-   for x in range(len(pl_array)):
-       if x = last_row:
-           if pl_array[x] - pl_array[x+1] > greatest_change:
-           print("higher")
-           
+
 
